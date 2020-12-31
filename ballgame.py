351053f5d29dd43a9cd0740d82ef0c9fa2f16e53
calypsomatic <calypsomatic@gmail.com>
2020-12-31 16:40:14 -0500

    add walls

--- ballgame.py
+++ ballgame.py
@@ -6,7 +6,7 @@ display_width = 800
 display_height = 600
 red = pygame.Color(255, 0, 0)
 green = pygame.Color(0,255,0)
-black = pygame.Color(0,0,0)
+BLACK = pygame.Color(0,0,0)
 white = pygame.Color(255,255,255)
 blue = pygame.Color(0,0,255)
 
@@ -31,12 +31,12 @@ class BounceSprite(pygame.sprite.Sprite):
     def pos(self):
         return self.rect.centerx, self.rect.centery
 
-    def aim_at_click(self, x, y):
+    def aim(self, pos):
+        x, y = pos
         dx = x - self.rect.centerx
         dy = y - self.rect.centery
         norm = (dx**2 + dy**2)**.5
         self.v = [int(10*dx/norm), int(10*dy/norm)]
-
 
     def move(self):
         dx, dy = self.v
@@ -56,6 +56,21 @@ class BounceSprite(pygame.sprite.Sprite):
     def reverse_v(self):
         vx, vy = self.v
         self.v = [-vx, -vy]
+
+    def flee(self, pos):
+        x,y = pos
+        dx = x - self.rect.centerx
+        dy = y - self.rect.centery
+        norm = (dx**2 + dy**2)**.5
+        self.v = [-int(10*dx/norm), -int(10*dy/norm)]
+
+    def reverse_x(self):
+        vx, vy = self.v
+        self.v = [-vx, vy]
+
+    def reverse_y(self):
+        vx, vy = self.v
+        self.v = [vx, -vy]
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, pos, imagefile):
@@ -96,23 +111,33 @@ class Sprite(pygame.sprite.Sprite):
         self.v = [dx,dy]
         self.move()
 
-##class WallSprite(pygame.sprite.Sprite):
-##    def __init__(self, pos, color):
-##        pygame.sprite.Sprite.__init__(self)
-##        self.image = pygame.Surface((size*2, size*2), pygame.SRCALPHA)
-##        pygame.draw.rect(self.image, (255, 0, 0), (size,size), size)
-##        self.rect = pygame.Rect(*gameDisplay.get_rect().center, 0,0).inflate(size, size)
-##        self.rect.center = pos
-##        self.rect.center = pos
-##        self.v = [0,0]
+    def reverse_x(self):
+        vx, vy = self.v
+        self.v = [-vx, vy]
+
+    def reverse_y(self):
+        vx, vy = self.v
+        self.v = [vx, -vy]
+
+class VerticalWallSprite(pygame.sprite.Sprite):
+    def __init__(self, height, pos, color=BLACK):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((5, height), pygame.SRCALPHA)
+        self.image.fill(color)
+        self.rect = self.image.get_bounding_rect()
+        self.rect.topleft = pos
+
+class HorizontalWallSprite(pygame.sprite.Sprite):
+    def __init__(self, width, pos, color=BLACK):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((width, 5), pygame.SRCALPHA)
+        self.image.fill(color)
+        self.rect = self.image.get_bounding_rect()
+        self.rect.topleft = pos
 
 
-##def item(img, x,y):
-##    gameDisplay.blit(img, (x,y))
-
-
-x = int(display_width * 0.45)
-y = int(display_height * 0.8)
+x = int(display_width * 0.05)
+y = int(display_height * 0.1)
 dx = -1
 dy = -1
 
@@ -126,13 +151,25 @@ sprite_group = pygame.sprite.Group()
 sprite_group.add(chickensprite)
 
 bounce_group = pygame.sprite.Group()
+bounce_group.add(chickensprite)
+
+ball_group = pygame.sprite.Group()
 
 
 def shootBall(x, y):
     ball = BounceSprite((display_width//2, display_height), 10)
-    ball.aim_at_click(mousex, mousey)
+    ball.aim((mousex, mousey))
     bounce_group.add(ball)
+    ball_group.add(ball)
 
+leftwall = VerticalWallSprite(display_height, (0,0))
+topwall = HorizontalWallSprite(display_width, (0,0))
+rightwall = VerticalWallSprite(display_height, (display_width-5,0))
+
+wall_group = pygame.sprite.Group()
+wall_group.add(leftwall)
+wall_group.add(topwall)
+wall_group.add(rightwall)
 
 while not crashed:
     for event in pygame.event.get():
@@ -150,25 +187,34 @@ while not crashed:
         chickensprite.moveToCursor()
     else:
         chickensprite.move()
-            
-    bounce_group.update()
 
-    collide = pygame.sprite.spritecollide(chickensprite, bounce_group, False)
+    collide = pygame.sprite.spritecollide(chickensprite, ball_group, False)
     for ball in collide:
-        ball.reverse_v()
+        ball.flee(chickensprite.pos())
+
+    leftwallcollide = pygame.sprite.spritecollide(leftwall, bounce_group, False)
+    for sprite in collide:
+        print(type(sprite))
+        sprite.reverse_x()
+    
+    topwallcollide = pygame.sprite.spritecollide(topwall, bounce_group, False)
+    for sprite in collide:
+        print(type(sprite))
+        sprite.reverse_y()
+        
+    rightwallcollide = pygame.sprite.spritecollide(rightwall, bounce_group, False)
+    for sprite in collide:
+        print(type(sprite))
+        sprite.reverse_x()
+        
 
     gameDisplay.fill(blue)
-    sprite_group.draw(gameDisplay)
+    bounce_group.update()
+    ball_group.update()
+##    sprite_group.draw(gameDisplay)
     bounce_group.draw(gameDisplay)
+    wall_group.draw(gameDisplay)
     
-    
-    
-##    item(ballImg,x,y)
-    
-##    x,y,dx,dy = move(x,y,dx,dy)
-
-#    pygame.draw.polygon(gameDisplay, color1, [(100,100), (150,150),(200,250),(350,100)], 1)
-
     pygame.display.update()
     clock.tick(20)
 
