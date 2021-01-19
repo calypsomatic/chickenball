45b4a0be1339cdb890807095818934563d208dab
calypsomatic <calypsomatic@gmail.com>
2021-01-19 12:48:50 -0500

    just some cleanup/mild refactoring

--- ballgame.py
+++ ballgame.py
@@ -5,16 +5,16 @@ import random
 
 pygame.init()
 
+## DEFINE CONSTANTS ##
 
-
+#COLORS
 RED = pygame.Color(255, 0, 0)
 GREEN = pygame.Color(0,255,0)
 BLACK = pygame.Color(0,0,0)
 WHITE = pygame.Color(255,255,255)
 BLUE = pygame.Color(0,0,255)
-PHASE = "AIMING"
-BALL_LIMIT = 30
 
+#CHICKEN PARAMETERS
 CHICKEN_IMAGE = pygame.image.load('chicksmall.png')
 CHICKEN_WIDTH = CHICKEN_IMAGE.get_rect().width
 CHICKEN_HEIGHT = CHICKEN_IMAGE.get_rect().height
@@ -22,29 +22,35 @@ CHICKEN_SPACING = 5
 CHICKEN_HP = 50
 CHICKEN_NUMBER = 8
 
+# DISPLAY PARAMETERS
 VW_WIDTH = 40
 HW_HEIGHT = 50
-
 ARENA_WIDTH = CHICKEN_NUMBER*(CHICKEN_WIDTH+CHICKEN_SPACING) + CHICKEN_SPACING
 ARENA_HEIGHT = HW_HEIGHT + 10*CHICKEN_HEIGHT
-
 DISPLAY_WIDTH = ARENA_WIDTH + 2*VW_WIDTH
 DISPLAY_HEIGHT = ARENA_HEIGHT + HW_HEIGHT
 
+#BALL PARAMETERS
 BALL_POS = (DISPLAY_WIDTH//2, DISPLAY_HEIGHT)
+BALL_LIMIT = 30
+BALL_SPEED = 3
+BALL_SIZE = 5
 
 WAVE = 0
+PHASE = "AIMING"
 
+#INITIALIZE DISPLAY
 gameDisplay = pygame.display.set_mode((DISPLAY_WIDTH,DISPLAY_HEIGHT))
 pygame.display.set_caption("Chicken Ball Game")
 clock = pygame.time.Clock()
 
+#FONTS
 ##fonts = pygame.font.get_fonts()
-font = pygame.font.SysFont("garuda", 15)
+hp_font = pygame.font.SysFont("garuda", 15)
 
-font_img = font.render("text", True, RED)
 
-class BounceSprite(pygame.sprite.Sprite):
+## DEFINE SPRITES ##
+class BallSprite(pygame.sprite.Sprite):
     def __init__(self, pos, radius):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((radius*2, radius*2), pygame.SRCALPHA)
@@ -53,7 +59,7 @@ class BounceSprite(pygame.sprite.Sprite):
         self.rect.center = pos
         self.x, self.y = pos
         self.v = [0,0]
-        self.speed = 3
+        self.speed = BALL_SPEED
         self.live = True
 
     def set_pos(self, x, y):
@@ -84,14 +90,6 @@ class BounceSprite(pygame.sprite.Sprite):
     def update(self):
         self.move()
 
-    def reverse_v(self):
-        vx, vy = self.v
-        self.v = [-vx, -vy] 
-
-    def flee(self, pos):
-        self.aim(pos)
-        self.reverse_v()
-
     def reverse_x(self):
         vx, vy = self.v
         self.v = [-vx, vy]
@@ -111,20 +109,20 @@ class BounceSprite(pygame.sprite.Sprite):
             self.reverse_y()
 
 
-class Sprite(pygame.sprite.Sprite):
+class ChickenSprite(pygame.sprite.Sprite):
     def __init__(self, pos, image, hp):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface.copy(image)
         self.rect = self.image.get_rect()
         self.rect.topleft = pos
         self.v = [0,0]
-        self.speed = 5
+#        self.speed = 5
         self.hp = hp
         self.live = True
         self.display_hp()
 
     def display_hp(self):
-        self.hp_text = font.render(str(self.hp), True, RED)
+        self.hp_text = hp_font.render(str(self.hp), True, RED)
         hp_surf = pygame.Surface((self.hp_text.get_rect().width, self.hp_text.get_rect().height), pygame.SRCALPHA)
         hp_surf.fill(WHITE)
         hp_surf.blit(self.hp_text, (0,0))
@@ -141,25 +139,25 @@ class Sprite(pygame.sprite.Sprite):
         x, y = self.pos()
         self.set_pos(x + dx, y + dy)
 
-    def aim(self, pos):
-        x, y = pos
-        dx = x - self.rect.centerx
-        dy = y - self.rect.centery
-        norm = (dx**2 + dy**2)**.5
-        self.v = [int(self.speed*dx/norm), int(self.speed*dy/norm)]
-
-        
-    def moveToCursor(self):
-        self.aim(pygame.mouse.get_pos())        
-        self.move()
-
-    def reverse_x(self):
-        vx, vy = self.v
-        self.v = [-vx, vy]
-
-    def reverse_y(self):
-        vx, vy = self.v
-        self.v = [vx, -vy]
+##    def aim(self, pos):
+##        x, y = pos
+##        dx = x - self.rect.centerx
+##        dy = y - self.rect.centery
+##        norm = (dx**2 + dy**2)**.5
+##        self.v = [int(self.speed*dx/norm), int(self.speed*dy/norm)]
+##
+##        
+##    def moveToCursor(self):
+##        self.aim(pygame.mouse.get_pos())        
+##        self.move()
+##
+##    def reverse_x(self):
+##        vx, vy = self.v
+##        self.v = [-vx, vy]
+##
+##    def reverse_y(self):
+##        vx, vy = self.v
+##        self.v = [vx, -vy]
 
     def take_hit(self, hit):
         self.hp = self.hp - hit
@@ -167,7 +165,6 @@ class Sprite(pygame.sprite.Sprite):
         if self.hp <= 0:
             self.kill()
             self.live = False
-
 
 
 class VerticalWallSprite(pygame.sprite.Sprite):
@@ -187,12 +184,21 @@ class HorizontalWallSprite(pygame.sprite.Sprite):
         self.rect.topleft = pos
 
 
-def addRandomChicken():
-    chicken_pos = int(100+random.random()*DISPLAY_WIDTH//2), int(100+random.random()*DISPLAY_HEIGHT//2)
-    addChicken(chicken_pos)
+##def addRandomChicken():
+##    chicken_pos = int(100+random.random()*DISPLAY_WIDTH//2), int(100+random.random()*DISPLAY_HEIGHT//2)
+##    addChicken(chicken_pos)
 
+
+## DEFINE SPRITE GROUPS ##
+sprite_group = pygame.sprite.Group()
+bounce_group = pygame.sprite.Group()
+ball_group = pygame.sprite.Group()
+target_group = pygame.sprite.Group()
+
+## DEFINE FUNCTIONS ##
+        
 def addChicken(pos):
-    chickensprite = Sprite(pos,CHICKEN_IMAGE, CHICKEN_HP)
+    chickensprite = ChickenSprite(pos,CHICKEN_IMAGE, CHICKEN_HP)
     sprite_group.add(chickensprite)
     bounce_group.add(chickensprite)
     target_group.add(chickensprite)
@@ -203,43 +209,39 @@ def spawnChickens():
     for i in spots:
         addChicken((VW_WIDTH + CHICKEN_SPACING + i*(CHICKEN_WIDTH + CHICKEN_SPACING),HW_HEIGHT + CHICKEN_SPACING))
 
-
-sprite_group = pygame.sprite.Group()
-bounce_group = pygame.sprite.Group()
-ball_group = pygame.sprite.Group()
-target_group = pygame.sprite.Group()
-
-spawnChickens()
-
-crashed = False
-buttondown = False
-
-
 def shootBall(x, y):
-    ball = BounceSprite(BALL_POS, 5)
+    ball = BallSprite(BALL_POS, BALL_SIZE)
     ball.aim((x, y))
     bounce_group.add(ball)
     ball_group.add(ball)
     sprite_group.add(ball)
 
-leftwall = VerticalWallSprite(DISPLAY_HEIGHT, (0,0))
-topwall = HorizontalWallSprite(DISPLAY_WIDTH, (0,0))
-rightwall = VerticalWallSprite(DISPLAY_HEIGHT, (DISPLAY_WIDTH-VW_WIDTH,0))
-
-wall_group = pygame.sprite.Group()
-wall_group.add(leftwall)
-wall_group.add(topwall)
-wall_group.add(rightwall)
-pygame.mouse.set_cursor(*pygame.cursors.diamond)
-i=0
-
 def drawTrackingLine(from_x):
     mousex, mousey = pygame.mouse.get_pos()
     pygame.draw.line(gameDisplay,BLACK, (from_x, DISPLAY_HEIGHT), (mousex, mousey))
 
-source_ball = BounceSprite((BALL_POS[0], DISPLAY_HEIGHT - 7), 5)
+## INITIALIZE SPRITES ##
+leftwall = VerticalWallSprite(DISPLAY_HEIGHT, (0,0))
+topwall = HorizontalWallSprite(DISPLAY_WIDTH, (0,0))
+rightwall = VerticalWallSprite(DISPLAY_HEIGHT, (DISPLAY_WIDTH-VW_WIDTH,0))
+wall_group = pygame.sprite.Group()
+wall_group.add(leftwall)
+wall_group.add(topwall)
+wall_group.add(rightwall)
+
+source_ball = BallSprite((BALL_POS[0], DISPLAY_HEIGHT - 7), BALL_SIZE)
 wall_group.add(source_ball)
 
+spawnChickens()
+
+## INITIALIZE VARIABLES ##
+
+pygame.mouse.set_cursor(*pygame.cursors.diamond)
+i=0
+crashed = False
+buttondown = False
+
+## START GAME ##
 while not crashed:
     i += 1
     fire = False
@@ -252,11 +254,11 @@ while not crashed:
         if event.type == pygame.MOUSEBUTTONUP:
             buttondown = False
             fire = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_c:
-                addRandomChicken()
+##        if event.type == pygame.KEYDOWN:
+##            if event.key == pygame.K_c:
+##                addRandomChicken()
 
-
+    # DETECT COLLISIONS #
     ballchickencollide = pygame.sprite.groupcollide(target_group, ball_group, False, False)
     for target, balls in ballchickencollide.items():
         for ball in balls:
@@ -275,14 +277,16 @@ while not crashed:
     for sprite in rightwallcollide:
         sprite.reverse_x()
 
-    
+    # remove dead sprites
     sprite_group.update()
     for sprite in sprite_group:
         if not sprite.live:
             del sprite
 
+    # background must be drawn before action so tracking line can appear
     gameDisplay.fill(BLUE)
 
+    # determine phase and take appropriate action
     if PHASE == 'AIMING' and buttondown:
         drawTrackingLine(BALL_POS[0])
     elif PHASE == 'AIMING' and fire:
@@ -291,12 +295,13 @@ while not crashed:
         remainingBalls = BALL_LIMIT
         aim_pos = pygame.mouse.get_pos()
     if PHASE == 'SHOOTING' and remainingBalls > 0:
+        # space out the balls
         if i % 5 == 0:
             shootBall(*aim_pos)
             remainingBalls -= 1
     elif PHASE == 'SHOOTING' and len(ball_group) == 0:
         PHASE = 'ADVANCING'
-        source_ball = BounceSprite((BALL_POS[0], DISPLAY_HEIGHT - 7), 5)
+        source_ball = BallSprite((BALL_POS[0], DISPLAY_HEIGHT - 7), BALL_SIZE)
         wall_group.add(source_ball)
         remainingSteps = CHICKEN_HEIGHT + CHICKEN_SPACING
         WAVE += 1
@@ -308,7 +313,7 @@ while not crashed:
         spawnChickens()
         PHASE = 'AIMING'
         
-        
+    # draw sprites #
     bounce_group.draw(gameDisplay)
     wall_group.draw(gameDisplay)
     
